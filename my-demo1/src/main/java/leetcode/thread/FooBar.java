7adb52dc39347eabc4ee6a4fbf6e941be30ed6f8
wh <13282027326@163.com>
2019-09-10 11:21:12 +0800

    leetcode线程题

--- my-demo1/src/main/java/leetcode/thread/FooBar.java
+++ my-demo1/src/main/java/leetcode/thread/FooBar.java
@@ -2,6 +2,7 @@ package leetcode.thread;
 
 import org.junit.Test;
 
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
 
@@ -12,26 +13,68 @@ public class FooBar {
 
     public FooBar(int n) {
         this.n = n;
+        cyclicBarrier = new CyclicBarrier(2);
+        countDownLatch = new CountDownLatch(1);
     }
+
 
     public void foo(Runnable printFoo) throws InterruptedException {
 
         for (int i = 0; i < n; i++) {
 
-            printFoo.run();
+            try {
+
+                printFoo.run();
+                countDownLatch.countDown();
+                cyclicBarrier.await();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public void bar(Runnable printBar) throws InterruptedException {
 
         for (int i = 0; i < n; i++) {
+            try {
 
-            printBar.run();
+                countDownLatch.await();
+                printBar.run();
+                countDownLatch = new CountDownLatch(1);
+                cyclicBarrier.await();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    @Test
-    public void showThread() {
+    public static void main(String[] args) {
+
+
+        FooBar f = new FooBar(10);
+
+        Thread t1 = new Thread(() -> {
+            try {
+                f.foo(() -> {
+                    System.out.print("foo");
+                });
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            try {
+                f.bar(() -> {
+                    System.out.println("bar");
+                });
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        t1.start();
+        t2.start();
 
     }
 }
